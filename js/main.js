e58e2d49a7e885f88a94ba327c66232d338897a5
Melisa500 <melisabarrera24@gmail.com>
2023-02-19 14:20:00 -0300

    ul

--- js/main.js
+++ js/main.js
@@ -4,10 +4,17 @@ const ingPorcentaje = document.getElementById("ingresoPorcentaje");
 const divid = document.getElementById("dividir");
 
 function moneda(value){
+    console.log(value);
+    value = Math.ceil(value * 100) / 100;
+    console.log(value);
     value = value.toFixed(2);
     return "$ " + value;
 }
 
+function pluralSingular(value){
+    if(value === 1) return value + " persona";
+    return value + " personas";
+}
 
 function tomarMonto(){
     let mon = Number(montoIngresado.value);
@@ -18,12 +25,10 @@ function tomarMonto(){
     let calculoCadaPorcentaje = calculoPorcentaje / div;
     let calculoCadaPersona = mon  / div;
 
-    console.log(calculoCadaPersona);
-
     document.getElementById("seleccionPorcentaje").innerHTML = por + "%";
     document.getElementById("totalPorcentaje").innerHTML = moneda(calculoPorcentaje);
     document.getElementById("total").innerHTML = moneda(mon + calculoPorcentaje);
-    document.getElementById("divisor").innerHTML = div + " personas";
+    document.getElementById("divisor").innerHTML = pluralSingular(div);
     document.getElementById("montoDiv").innerHTML = moneda(calculoCadaPersona);
     document.getElementById("porcentajeDiv").innerHTML = moneda(calculoCadaPorcentaje);
     document.getElementById("sumaTotal").innerHTML = moneda(calculoCadaPersona + calculoCadaPorcentaje);
